21ffaf27d113a51e21a179d2fff05a28ad4f7040
Reiko Kitsune <exvoided@gmail.com>
2016-03-01 11:59:44 +0300

    ExportImage refactoring. Using now chars 'X' and '.' instead of '1' and '0'.

--- src/NFirmwareEditor/Firmware/ExportedImage.cs
+++ src/NFirmwareEditor/Firmware/ExportedImage.cs
@@ -6,6 +6,8 @@ namespace NFirmwareEditor.Firmware
 {
 	public class ExportedImage
 	{
+		private const char TrueChar = 'X';
+		private const char FalseChar = '.';
 		private bool[,] m_data;
 
 		public ExportedImage()
@@ -32,42 +34,53 @@ namespace NFirmwareEditor.Firmware
 		public int Height { get; set; }
 
 		[XmlElement("Image")]
-		public string ImagePixels { get; set; }
+		public string ImageAsciiString { get; set; }
 
 		[XmlIgnore]
 		public bool[,] Data
 		{
-			get
-			{
-				var result = new bool[Width, Height];
-				if (string.IsNullOrEmpty(ImagePixels)) return result;
-
-				var rows = ImagePixels.Trim().Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-				for (var row = 0; row < rows.Length; row++)
-				{
-					for (var col = 0; col < rows[row].Length; col++)
-					{
-						result[col, row] = rows[row][col].Equals('1');
-					}
-				}
-				return result;
-			}
+			get { return ReadImageFromAsciiString(Width, Height, ImageAsciiString); }
 			set
 			{
 				m_data = value;
-
-				var sb = new StringBuilder();
-				sb.AppendLine();
-				for (var row = 0; row < Height; row++)
-				{
-					for (var col = 0; col < Width; col++)
-					{
-						sb.Append(m_data[col, row] ? "1" : "0");
-					}
-					sb.AppendLine();
-				}
-				ImagePixels = sb.ToString();
+				ImageAsciiString = WriteImageToAsciiString(Width, Height, m_data);
 			}
+		}
+
+		private static bool[,] ReadImageFromAsciiString(int width, int height, string text)
+		{
+			var result = new bool[width, height];
+			if (string.IsNullOrEmpty(text)) return result;
+
+			var rows = text.Trim().Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			for (var row = 0; row < rows.Length; row++)
+			{
+				for (var col = 0; col < rows[row].Length; col++)
+				{
+					result[col, row] = IsTrue(rows[row][col]);
+				}
+			}
+			return result;
+		}
+
+		private static string WriteImageToAsciiString(int width, int height, bool[,] data)
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine();
+			for (var row = 0; row < height; row++)
+			{
+				for (var col = 0; col < width; col++)
+				{
+					sb.Append(data[col, row] ? TrueChar : FalseChar);
+				}
+				sb.AppendLine();
+			}
+			return sb.ToString();
+		}
+
+		private static bool IsTrue(char c)
+		{
+			return c.Equals(TrueChar) || c.Equals('1');
 		}
 	}
 }
