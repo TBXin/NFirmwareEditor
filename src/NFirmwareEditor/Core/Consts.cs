116e92b9d42e0db629ce27fe6f35504bb3513966
ReikoKitsune <exvoided@gmail.com>
2016-03-08 01:16:14 +0300

    Update app version.

--- src/NFirmwareEditor/Core/Consts.cs
+++ src/NFirmwareEditor/Core/Consts.cs
@@ -3,7 +3,7 @@
 	internal static class Consts
 	{
 		internal const string ApplicationTitle = "NFirmwareEditor";
-		internal const string ApplicationVersion = "1.8";
+		internal const string ApplicationVersion = "2.0";
 		internal const string FirmwareFilter = "Firmware file|*.bin";
 		internal const string ExportImageFilter = "Exported images|*.images";
 		internal const string Encrypted = "encrypted";
