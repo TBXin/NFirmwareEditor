f55f38a3626944e8402bea627a603f1701e345b4
Reiko Kitsune <exvoided@gmail.com>
2016-03-30 17:14:46 +0300

    Ref.

--- src/NFirmware.Tests/FirmwareStreamTests.cs
+++ src/NFirmware.Tests/FirmwareStreamTests.cs
@@ -5,7 +5,7 @@ namespace NFirmware.Tests
 	[TestClass]
 	public class FirmwareStreamTests
 	{
-		private byte[] GetDummy()
+		private static byte[] GetDummy()
 		{
 			return new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		}
