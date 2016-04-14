8a54bdd57f46ce83ed1d433c3dba22b85797f605
Reiko Kitsune <exvoided@gmail.com>
2016-04-14 12:58:42 +0300

    Version update.

--- src/NFirmwareEditor/Core/Consts.cs
+++ src/NFirmwareEditor/Core/Consts.cs
@@ -2,7 +2,7 @@
 {
 	internal static class Consts
 	{
-		internal const string ApplicationVersion = "3.5";
+		internal const string ApplicationVersion = "3.51";
 		internal const string ApplicationTitleWoVersion = "NFirmwareEditor";
 		internal const string ApplicationTitle = ApplicationTitleWoVersion + " v" + ApplicationVersion;
 
