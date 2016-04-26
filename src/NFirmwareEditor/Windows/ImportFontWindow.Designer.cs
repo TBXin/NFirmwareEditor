d52c1788a3198c9f681553f321faf045c4705867
ReikoKitsune <exvoided@gmail.com>
2016-04-26 18:07:44 +0300

    Import font window layout improvement.

--- src/NFirmwareEditor/Windows/ImportFontWindow.Designer.cs
+++ src/NFirmwareEditor/Windows/ImportFontWindow.Designer.cs
@@ -33,7 +33,6 @@
 			this.label3 = new System.Windows.Forms.Label();
 			this.OkButton = new System.Windows.Forms.Button();
 			this.CancelButton = new System.Windows.Forms.Button();
-			this.borderedPanel2 = new NFirmwareEditor.UI.BorderedPanel();
 			this.UpperAZButton = new System.Windows.Forms.Button();
 			this.LowerAZButton = new System.Windows.Forms.Button();
 			this.ZeroNineButton = new System.Windows.Forms.Button();
@@ -50,14 +49,13 @@
 			this.label4 = new System.Windows.Forms.Label();
 			this.FontComboBox = new System.Windows.Forms.ComboBox();
 			this.label2 = new System.Windows.Forms.Label();
-			this.borderedPanel4 = new NFirmwareEditor.UI.BorderedPanel();
 			this.FontPreviewSurface = new NFirmwareEditor.UI.DrawingSurface();
 			this.label1 = new System.Windows.Forms.Label();
 			this.MainToolTip = new System.Windows.Forms.ToolTip(this.components);
+			this.groupPanel1 = new NFirmwareEditor.UI.GroupPanel();
 			this.ControlBorderedPanel.SuspendLayout();
-			this.borderedPanel2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.FontSizeUpDown)).BeginInit();
-			this.borderedPanel4.SuspendLayout();
+			this.groupPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// ControlBorderedPanel
@@ -116,47 +114,10 @@
 			this.CancelButton.Text = "Cancel";
 			this.CancelButton.UseVisualStyleBackColor = true;
 			// 
-			// borderedPanel2
-			// 
-			this.borderedPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.borderedPanel2.BackColor = System.Drawing.Color.White;
-			this.borderedPanel2.BorderBottom = true;
-			this.borderedPanel2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
-			this.borderedPanel2.BorderLeft = true;
-			this.borderedPanel2.BorderRight = true;
-			this.borderedPanel2.BorderTop = true;
-			this.borderedPanel2.BorderWidth = 1F;
-			this.borderedPanel2.Controls.Add(this.UpperAZButton);
-			this.borderedPanel2.Controls.Add(this.LowerAZButton);
-			this.borderedPanel2.Controls.Add(this.ZeroNineButton);
-			this.borderedPanel2.Controls.Add(this.UnderlineButton);
-			this.borderedPanel2.Controls.Add(this.ItalicButton);
-			this.borderedPanel2.Controls.Add(this.BoldButton);
-			this.borderedPanel2.Controls.Add(this.LoadFontButton);
-			this.borderedPanel2.Controls.Add(this.ShiftDownButton);
-			this.borderedPanel2.Controls.Add(this.ShiftUpButton);
-			this.borderedPanel2.Controls.Add(this.ShiftRightButton);
-			this.borderedPanel2.Controls.Add(this.ShiftLeftButton);
-			this.borderedPanel2.Controls.Add(this.FontSizeUpDown);
-			this.borderedPanel2.Controls.Add(this.LettersTextBox);
-			this.borderedPanel2.Controls.Add(this.label4);
-			this.borderedPanel2.Controls.Add(this.FontComboBox);
-			this.borderedPanel2.Controls.Add(this.label2);
-			this.borderedPanel2.Controls.Add(this.borderedPanel4);
-			this.borderedPanel2.Controls.Add(this.label1);
-			this.borderedPanel2.Location = new System.Drawing.Point(3, 3);
-			this.borderedPanel2.Name = "borderedPanel2";
-			this.borderedPanel2.Padding = new System.Windows.Forms.Padding(1);
-			this.borderedPanel2.Size = new System.Drawing.Size(828, 311);
-			this.borderedPanel2.TabIndex = 0;
-			this.borderedPanel2.Text = "borderedPanel2";
-			// 
 			// UpperAZButton
 			// 
 			this.UpperAZButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.UpperAZButton.Location = new System.Drawing.Point(790, 4);
+			this.UpperAZButton.Location = new System.Drawing.Point(790, 3);
 			this.UpperAZButton.Name = "UpperAZButton";
 			this.UpperAZButton.Size = new System.Drawing.Size(35, 23);
 			this.UpperAZButton.TabIndex = 18;
@@ -167,7 +128,7 @@
 			// LowerAZButton
 			// 
 			this.LowerAZButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.LowerAZButton.Location = new System.Drawing.Point(756, 4);
+			this.LowerAZButton.Location = new System.Drawing.Point(756, 3);
 			this.LowerAZButton.Name = "LowerAZButton";
 			this.LowerAZButton.Size = new System.Drawing.Size(35, 23);
 			this.LowerAZButton.TabIndex = 17;
@@ -178,7 +139,7 @@
 			// ZeroNineButton
 			// 
 			this.ZeroNineButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.ZeroNineButton.Location = new System.Drawing.Point(722, 4);
+			this.ZeroNineButton.Location = new System.Drawing.Point(722, 3);
 			this.ZeroNineButton.Name = "ZeroNineButton";
 			this.ZeroNineButton.Size = new System.Drawing.Size(35, 23);
 			this.ZeroNineButton.TabIndex = 16;
@@ -190,7 +151,7 @@
 			// 
 			this.UnderlineButton.Appearance = System.Windows.Forms.Appearance.Button;
 			this.UnderlineButton.Image = global::NFirmwareEditor.Properties.Resources.font_underline;
-			this.UnderlineButton.Location = new System.Drawing.Point(349, 4);
+			this.UnderlineButton.Location = new System.Drawing.Point(349, 3);
 			this.UnderlineButton.Name = "UnderlineButton";
 			this.UnderlineButton.Size = new System.Drawing.Size(24, 23);
 			this.UnderlineButton.TabIndex = 15;
@@ -202,7 +163,7 @@
 			// 
 			this.ItalicButton.Appearance = System.Windows.Forms.Appearance.Button;
 			this.ItalicButton.Image = global::NFirmwareEditor.Properties.Resources.font_italic;
-			this.ItalicButton.Location = new System.Drawing.Point(326, 4);
+			this.ItalicButton.Location = new System.Drawing.Point(326, 3);
 			this.ItalicButton.Name = "ItalicButton";
 			this.ItalicButton.Size = new System.Drawing.Size(24, 23);
 			this.ItalicButton.TabIndex = 14;
@@ -214,7 +175,7 @@
 			// 
 			this.BoldButton.Appearance = System.Windows.Forms.Appearance.Button;
 			this.BoldButton.Image = global::NFirmwareEditor.Properties.Resources.font_bold;
-			this.BoldButton.Location = new System.Drawing.Point(303, 4);
+			this.BoldButton.Location = new System.Drawing.Point(303, 3);
 			this.BoldButton.Name = "BoldButton";
 			this.BoldButton.Size = new System.Drawing.Size(24, 23);
 			this.BoldButton.TabIndex = 13;
@@ -225,7 +186,7 @@
 			// LoadFontButton
 			// 
 			this.LoadFontButton.Image = global::NFirmwareEditor.Properties.Resources.font_load;
-			this.LoadFontButton.Location = new System.Drawing.Point(273, 4);
+			this.LoadFontButton.Location = new System.Drawing.Point(273, 3);
 			this.LoadFontButton.Name = "LoadFontButton";
 			this.LoadFontButton.Size = new System.Drawing.Size(24, 23);
 			this.LoadFontButton.TabIndex = 12;
@@ -235,7 +196,7 @@
 			// ShiftDownButton
 			// 
 			this.ShiftDownButton.Image = global::NFirmwareEditor.Properties.Resources.arrow_down;
-			this.ShiftDownButton.Location = new System.Drawing.Point(448, 4);
+			this.ShiftDownButton.Location = new System.Drawing.Point(448, 3);
 			this.ShiftDownButton.Name = "ShiftDownButton";
 			this.ShiftDownButton.Size = new System.Drawing.Size(24, 23);
 			this.ShiftDownButton.TabIndex = 7;
@@ -246,7 +207,7 @@
 			// ShiftUpButton
 			// 
 			this.ShiftUpButton.Image = global::NFirmwareEditor.Properties.Resources.arrow_up;
-			this.ShiftUpButton.Location = new System.Drawing.Point(425, 4);
+			this.ShiftUpButton.Location = new System.Drawing.Point(425, 3);
 			this.ShiftUpButton.Name = "ShiftUpButton";
 			this.ShiftUpButton.Size = new System.Drawing.Size(24, 23);
 			this.ShiftUpButton.TabIndex = 6;
@@ -257,7 +218,7 @@
 			// ShiftRightButton
 			// 
 			this.ShiftRightButton.Image = global::NFirmwareEditor.Properties.Resources.arrow_right;
-			this.ShiftRightButton.Location = new System.Drawing.Point(402, 4);
+			this.ShiftRightButton.Location = new System.Drawing.Point(402, 3);
 			this.ShiftRightButton.Name = "ShiftRightButton";
 			this.ShiftRightButton.Size = new System.Drawing.Size(24, 23);
 			this.ShiftRightButton.TabIndex = 5;
@@ -268,7 +229,7 @@
 			// ShiftLeftButton
 			// 
 			this.ShiftLeftButton.Image = global::NFirmwareEditor.Properties.Resources.arrow_left;
-			this.ShiftLeftButton.Location = new System.Drawing.Point(379, 4);
+			this.ShiftLeftButton.Location = new System.Drawing.Point(379, 3);
 			this.ShiftLeftButton.Name = "ShiftLeftButton";
 			this.ShiftLeftButton.Size = new System.Drawing.Size(24, 23);
 			this.ShiftLeftButton.TabIndex = 4;
@@ -278,7 +239,7 @@
 			// 
 			// FontSizeUpDown
 			// 
-			this.FontSizeUpDown.Location = new System.Drawing.Point(56, 5);
+			this.FontSizeUpDown.Location = new System.Drawing.Point(56, 4);
 			this.FontSizeUpDown.Maximum = new decimal(new int[] {
             72,
             0,
@@ -302,7 +263,7 @@
 			// 
 			this.LettersTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.LettersTextBox.Location = new System.Drawing.Point(515, 5);
+			this.LettersTextBox.Location = new System.Drawing.Point(515, 4);
 			this.LettersTextBox.Name = "LettersTextBox";
 			this.LettersTextBox.Size = new System.Drawing.Size(207, 21);
 			this.LettersTextBox.TabIndex = 0;
@@ -310,7 +271,7 @@
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(472, 8);
+			this.label4.Location = new System.Drawing.Point(472, 7);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(45, 13);
 			this.label4.TabIndex = 11;
@@ -321,7 +282,7 @@
 			this.FontComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
 			this.FontComboBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
 			this.FontComboBox.FormattingEnabled = true;
-			this.FontComboBox.Location = new System.Drawing.Point(133, 5);
+			this.FontComboBox.Location = new System.Drawing.Point(133, 4);
 			this.FontComboBox.Name = "FontComboBox";
 			this.FontComboBox.Size = new System.Drawing.Size(140, 21);
 			this.FontComboBox.TabIndex = 2;
@@ -329,58 +290,72 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(102, 8);
+			this.label2.Location = new System.Drawing.Point(102, 6);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(33, 13);
 			this.label2.TabIndex = 8;
 			this.label2.Text = "Font:";
-			// 
-			// borderedPanel4
-			// 
-			this.borderedPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-			this.borderedPanel4.BackColor = System.Drawing.Color.Transparent;
-			this.borderedPanel4.BorderBottom = false;
-			this.borderedPanel4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
-			this.borderedPanel4.BorderLeft = false;
-			this.borderedPanel4.BorderRight = false;
-			this.borderedPanel4.BorderTop = true;
-			this.borderedPanel4.BorderWidth = 1F;
-			this.borderedPanel4.Controls.Add(this.FontPreviewSurface);
-			this.borderedPanel4.Location = new System.Drawing.Point(1, 29);
-			this.borderedPanel4.Name = "borderedPanel4";
-			this.borderedPanel4.Padding = new System.Windows.Forms.Padding(0, 1, 0, 0);
-			this.borderedPanel4.Size = new System.Drawing.Size(826, 281);
-			this.borderedPanel4.TabIndex = 3;
-			this.borderedPanel4.Text = "borderedPanel4";
 			// 
 			// FontPreviewSurface
 			// 
 			this.FontPreviewSurface.AutoScroll = true;
 			this.FontPreviewSurface.BackgroundImage = global::NFirmwareEditor.Properties.Resources.transparent_bg;
 			this.FontPreviewSurface.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.FontPreviewSurface.Location = new System.Drawing.Point(0, 1);
+			this.FontPreviewSurface.Location = new System.Drawing.Point(1, 29);
 			this.FontPreviewSurface.Name = "FontPreviewSurface";
-			this.FontPreviewSurface.Size = new System.Drawing.Size(826, 280);
+			this.FontPreviewSurface.Size = new System.Drawing.Size(826, 281);
 			this.FontPreviewSurface.TabIndex = 0;
 			this.FontPreviewSurface.Text = "drawingSurface1";
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(4, 8);
+			this.label1.Location = new System.Drawing.Point(4, 7);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(54, 13);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Font size:";
+			// 
+			// groupPanel1
+			// 
+			this.groupPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupPanel1.BackColor = System.Drawing.Color.White;
+			this.groupPanel1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
+			this.groupPanel1.Controls.Add(this.LettersTextBox);
+			this.groupPanel1.Controls.Add(this.FontComboBox);
+			this.groupPanel1.Controls.Add(this.FontSizeUpDown);
+			this.groupPanel1.Controls.Add(this.UpperAZButton);
+			this.groupPanel1.Controls.Add(this.FontPreviewSurface);
+			this.groupPanel1.Controls.Add(this.LowerAZButton);
+			this.groupPanel1.Controls.Add(this.label1);
+			this.groupPanel1.Controls.Add(this.ZeroNineButton);
+			this.groupPanel1.Controls.Add(this.label2);
+			this.groupPanel1.Controls.Add(this.UnderlineButton);
+			this.groupPanel1.Controls.Add(this.ItalicButton);
+			this.groupPanel1.Controls.Add(this.label4);
+			this.groupPanel1.Controls.Add(this.BoldButton);
+			this.groupPanel1.Controls.Add(this.LoadFontButton);
+			this.groupPanel1.Controls.Add(this.ShiftDownButton);
+			this.groupPanel1.Controls.Add(this.ShiftLeftButton);
+			this.groupPanel1.Controls.Add(this.ShiftUpButton);
+			this.groupPanel1.Controls.Add(this.ShiftRightButton);
+			this.groupPanel1.HeaderBackColor = System.Drawing.Color.White;
+			this.groupPanel1.HeaderHeight = 29;
+			this.groupPanel1.Location = new System.Drawing.Point(3, 3);
+			this.groupPanel1.Name = "groupPanel1";
+			this.groupPanel1.Padding = new System.Windows.Forms.Padding(1, 0, 1, 1);
+			this.groupPanel1.Size = new System.Drawing.Size(828, 311);
+			this.groupPanel1.TabIndex = 4;
+			this.groupPanel1.TabStop = false;
 			// 
 			// ImportFontWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(834, 361);
-			this.Controls.Add(this.borderedPanel2);
+			this.Controls.Add(this.groupPanel1);
 			this.Controls.Add(this.ControlBorderedPanel);
 			this.Font = new System.Drawing.Font("Tahoma", 8.25F);
 			this.MaximizeBox = false;
@@ -390,10 +365,9 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "NFirmwareEditor - Import Font";
 			this.ControlBorderedPanel.ResumeLayout(false);
-			this.borderedPanel2.ResumeLayout(false);
-			this.borderedPanel2.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.FontSizeUpDown)).EndInit();
-			this.borderedPanel4.ResumeLayout(false);
+			this.groupPanel1.ResumeLayout(false);
+			this.groupPanel1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -403,12 +377,10 @@
 		private UI.BorderedPanel ControlBorderedPanel;
 		private System.Windows.Forms.Button OkButton;
 		private System.Windows.Forms.Button CancelButton;
-		private UI.BorderedPanel borderedPanel2;
 		private System.Windows.Forms.Button ShiftDownButton;
 		private System.Windows.Forms.Button ShiftUpButton;
 		private System.Windows.Forms.Button ShiftRightButton;
 		private System.Windows.Forms.Button ShiftLeftButton;
-		private UI.BorderedPanel borderedPanel4;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.NumericUpDown FontSizeUpDown;
 		private System.Windows.Forms.ComboBox FontComboBox;
@@ -425,5 +397,6 @@
 		private System.Windows.Forms.Button ZeroNineButton;
 		private System.Windows.Forms.ToolTip MainToolTip;
 		private System.Windows.Forms.Label label3;
+		private UI.GroupPanel groupPanel1;
 	}
 }
