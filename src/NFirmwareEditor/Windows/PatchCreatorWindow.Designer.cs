77a5aaa036d62fb74402504280749abd57a27f00
ReikoKitsune <exvoided@gmail.com>
2016-04-28 19:18:59 +0300

    Patch creator window layout update.

--- src/NFirmwareEditor/Windows/PatchCreatorWindow.Designer.cs
+++ src/NFirmwareEditor/Windows/PatchCreatorWindow.Designer.cs
@@ -28,7 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.groupBox1 = new NFirmwareEditor.UI.GroupPanel();
 			this.CreateDiffLink = new System.Windows.Forms.LinkLabel();
 			this.label8 = new System.Windows.Forms.Label();
 			this.DataTextBox = new System.Windows.Forms.TextBox();
@@ -38,7 +38,7 @@
 			this.label1 = new System.Windows.Forms.Label();
 			this.SelectSource1Button = new System.Windows.Forms.Button();
 			this.Source1TextBox = new System.Windows.Forms.TextBox();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new NFirmwareEditor.UI.GroupPanel();
 			this.DefinitionComboBox = new System.Windows.Forms.ComboBox();
 			this.label7 = new System.Windows.Forms.Label();
 			this.DescriptionTextBox = new System.Windows.Forms.TextBox();
@@ -59,9 +59,9 @@
 			// 
 			// groupBox1
 			// 
-			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
 			this.groupBox1.Controls.Add(this.CreateDiffLink);
 			this.groupBox1.Controls.Add(this.label8);
 			this.groupBox1.Controls.Add(this.DataTextBox);
@@ -71,9 +71,11 @@
 			this.groupBox1.Controls.Add(this.label1);
 			this.groupBox1.Controls.Add(this.SelectSource1Button);
 			this.groupBox1.Controls.Add(this.Source1TextBox);
-			this.groupBox1.Location = new System.Drawing.Point(3, 228);
+			this.groupBox1.HeaderBackColor = System.Drawing.Color.White;
+			this.groupBox1.HeaderHeight = 30;
+			this.groupBox1.Location = new System.Drawing.Point(3, 241);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(378, 342);
+			this.groupBox1.Size = new System.Drawing.Size(378, 280);
 			this.groupBox1.TabIndex = 1;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Patch data:";
@@ -82,7 +84,7 @@
 			// 
 			this.CreateDiffLink.AutoSize = true;
 			this.CreateDiffLink.Enabled = false;
-			this.CreateDiffLink.Location = new System.Drawing.Point(61, 77);
+			this.CreateDiffLink.Location = new System.Drawing.Point(61, 90);
 			this.CreateDiffLink.Name = "CreateDiffLink";
 			this.CreateDiffLink.Size = new System.Drawing.Size(41, 13);
 			this.CreateDiffLink.TabIndex = 26;
@@ -93,7 +95,7 @@
 			// label8
 			// 
 			this.label8.AutoSize = true;
-			this.label8.Location = new System.Drawing.Point(9, 77);
+			this.label8.Location = new System.Drawing.Point(9, 90);
 			this.label8.Name = "label8";
 			this.label8.Size = new System.Drawing.Size(55, 13);
 			this.label8.TabIndex = 25;
@@ -105,18 +107,18 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.DataTextBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-			this.DataTextBox.Location = new System.Drawing.Point(106, 74);
+			this.DataTextBox.Location = new System.Drawing.Point(106, 87);
 			this.DataTextBox.Multiline = true;
 			this.DataTextBox.Name = "DataTextBox";
 			this.DataTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.DataTextBox.Size = new System.Drawing.Size(267, 262);
+			this.DataTextBox.Size = new System.Drawing.Size(268, 189);
 			this.DataTextBox.TabIndex = 0;
 			this.DataTextBox.TabStop = false;
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(9, 50);
+			this.label2.Location = new System.Drawing.Point(9, 63);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(94, 13);
 			this.label2.TabIndex = 11;
@@ -125,9 +127,9 @@
 			// SelectSource2Button
 			// 
 			this.SelectSource2Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.SelectSource2Button.Location = new System.Drawing.Point(298, 47);
+			this.SelectSource2Button.Location = new System.Drawing.Point(300, 59);
 			this.SelectSource2Button.Name = "SelectSource2Button";
-			this.SelectSource2Button.Size = new System.Drawing.Size(75, 21);
+			this.SelectSource2Button.Size = new System.Drawing.Size(75, 23);
 			this.SelectSource2Button.TabIndex = 6;
 			this.SelectSource2Button.Text = "Select";
 			this.SelectSource2Button.UseVisualStyleBackColor = true;
@@ -136,17 +138,17 @@
 			// 
 			this.Source2TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.Source2TextBox.Location = new System.Drawing.Point(106, 47);
+			this.Source2TextBox.Location = new System.Drawing.Point(106, 60);
 			this.Source2TextBox.Name = "Source2TextBox";
 			this.Source2TextBox.ReadOnly = true;
-			this.Source2TextBox.Size = new System.Drawing.Size(186, 21);
+			this.Source2TextBox.Size = new System.Drawing.Size(188, 21);
 			this.Source2TextBox.TabIndex = 0;
 			this.Source2TextBox.TabStop = false;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(9, 23);
+			this.label1.Location = new System.Drawing.Point(9, 36);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(91, 13);
 			this.label1.TabIndex = 8;
@@ -155,9 +157,9 @@
 			// SelectSource1Button
 			// 
 			this.SelectSource1Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.SelectSource1Button.Location = new System.Drawing.Point(298, 20);
+			this.SelectSource1Button.Location = new System.Drawing.Point(300, 32);
 			this.SelectSource1Button.Name = "SelectSource1Button";
-			this.SelectSource1Button.Size = new System.Drawing.Size(75, 21);
+			this.SelectSource1Button.Size = new System.Drawing.Size(75, 23);
 			this.SelectSource1Button.TabIndex = 5;
 			this.SelectSource1Button.Text = "Select";
 			this.SelectSource1Button.UseVisualStyleBackColor = true;
@@ -166,10 +168,10 @@
 			// 
 			this.Source1TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.Source1TextBox.Location = new System.Drawing.Point(106, 20);
+			this.Source1TextBox.Location = new System.Drawing.Point(106, 33);
 			this.Source1TextBox.Name = "Source1TextBox";
 			this.Source1TextBox.ReadOnly = true;
-			this.Source1TextBox.Size = new System.Drawing.Size(186, 21);
+			this.Source1TextBox.Size = new System.Drawing.Size(188, 21);
 			this.Source1TextBox.TabIndex = 0;
 			this.Source1TextBox.TabStop = false;
 			// 
@@ -177,6 +179,7 @@
 			// 
 			this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(185)))), ((int)(((byte)(185)))), ((int)(((byte)(185)))));
 			this.groupBox2.Controls.Add(this.DefinitionComboBox);
 			this.groupBox2.Controls.Add(this.label7);
 			this.groupBox2.Controls.Add(this.DescriptionTextBox);
@@ -187,9 +190,11 @@
 			this.groupBox2.Controls.Add(this.VersionTextBox);
 			this.groupBox2.Controls.Add(this.label3);
 			this.groupBox2.Controls.Add(this.NameTextBox);
-			this.groupBox2.Location = new System.Drawing.Point(3, 1);
+			this.groupBox2.HeaderBackColor = System.Drawing.Color.White;
+			this.groupBox2.HeaderHeight = 30;
+			this.groupBox2.Location = new System.Drawing.Point(3, 3);
 			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(378, 221);
+			this.groupBox2.Size = new System.Drawing.Size(378, 232);
 			this.groupBox2.TabIndex = 0;
 			this.groupBox2.TabStop = false;
 			this.groupBox2.Text = "Patch info:";
@@ -199,15 +204,15 @@
 			this.DefinitionComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.DefinitionComboBox.FormattingEnabled = true;
-			this.DefinitionComboBox.Location = new System.Drawing.Point(106, 101);
+			this.DefinitionComboBox.Location = new System.Drawing.Point(106, 114);
 			this.DefinitionComboBox.Name = "DefinitionComboBox";
-			this.DefinitionComboBox.Size = new System.Drawing.Size(267, 21);
+			this.DefinitionComboBox.Size = new System.Drawing.Size(268, 21);
 			this.DefinitionComboBox.TabIndex = 3;
 			// 
 			// label7
 			// 
 			this.label7.AutoSize = true;
-			this.label7.Location = new System.Drawing.Point(9, 131);
+			this.label7.Location = new System.Drawing.Point(9, 144);
 			this.label7.Name = "label7";
 			this.label7.Size = new System.Drawing.Size(64, 13);
 			this.label7.TabIndex = 22;
@@ -215,19 +220,19 @@
 			// 
 			// DescriptionTextBox
 			// 
-			this.DescriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+			this.DescriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.DescriptionTextBox.Location = new System.Drawing.Point(106, 128);
+			this.DescriptionTextBox.Location = new System.Drawing.Point(106, 141);
 			this.DescriptionTextBox.Multiline = true;
 			this.DescriptionTextBox.Name = "DescriptionTextBox";
-			this.DescriptionTextBox.Size = new System.Drawing.Size(267, 87);
+			this.DescriptionTextBox.Size = new System.Drawing.Size(268, 87);
 			this.DescriptionTextBox.TabIndex = 4;
+			this.DescriptionTextBox.Text = "1\r\n2\r\n3\r\n4\r\n5\r\n6";
 			// 
 			// label6
 			// 
 			this.label6.AutoSize = true;
-			this.label6.Location = new System.Drawing.Point(9, 104);
+			this.label6.Location = new System.Drawing.Point(9, 117);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(56, 13);
 			this.label6.TabIndex = 20;
@@ -236,7 +241,7 @@
 			// label5
 			// 
 			this.label5.AutoSize = true;
-			this.label5.Location = new System.Drawing.Point(9, 77);
+			this.label5.Location = new System.Drawing.Point(9, 90);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(44, 13);
 			this.label5.TabIndex = 18;
@@ -246,15 +251,15 @@
 			// 
 			this.AuthorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.AuthorTextBox.Location = new System.Drawing.Point(106, 74);
+			this.AuthorTextBox.Location = new System.Drawing.Point(106, 87);
 			this.AuthorTextBox.Name = "AuthorTextBox";
-			this.AuthorTextBox.Size = new System.Drawing.Size(267, 21);
+			this.AuthorTextBox.Size = new System.Drawing.Size(268, 21);
 			this.AuthorTextBox.TabIndex = 2;
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
-			this.label4.Location = new System.Drawing.Point(9, 50);
+			this.label4.Location = new System.Drawing.Point(9, 63);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(46, 13);
 			this.label4.TabIndex = 16;
@@ -264,16 +269,16 @@
 			// 
 			this.VersionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.VersionTextBox.Location = new System.Drawing.Point(106, 47);
+			this.VersionTextBox.Location = new System.Drawing.Point(106, 60);
 			this.VersionTextBox.Name = "VersionTextBox";
-			this.VersionTextBox.Size = new System.Drawing.Size(267, 21);
+			this.VersionTextBox.Size = new System.Drawing.Size(268, 21);
 			this.VersionTextBox.TabIndex = 1;
 			this.VersionTextBox.Text = "1.0";
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(9, 23);
+			this.label3.Location = new System.Drawing.Point(9, 36);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(38, 13);
 			this.label3.TabIndex = 14;
@@ -283,9 +288,9 @@
 			// 
 			this.NameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.NameTextBox.Location = new System.Drawing.Point(106, 20);
+			this.NameTextBox.Location = new System.Drawing.Point(106, 33);
 			this.NameTextBox.Name = "NameTextBox";
-			this.NameTextBox.Size = new System.Drawing.Size(267, 21);
+			this.NameTextBox.Size = new System.Drawing.Size(268, 21);
 			this.NameTextBox.TabIndex = 0;
 			// 
 			// ControlBorderedPanel
@@ -300,7 +305,7 @@
 			this.ControlBorderedPanel.Controls.Add(this.OkButton);
 			this.ControlBorderedPanel.Controls.Add(this.CancelButton);
 			this.ControlBorderedPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.ControlBorderedPanel.Location = new System.Drawing.Point(0, 572);
+			this.ControlBorderedPanel.Location = new System.Drawing.Point(0, 524);
 			this.ControlBorderedPanel.Name = "ControlBorderedPanel";
 			this.ControlBorderedPanel.Padding = new System.Windows.Forms.Padding(0, 1, 0, 0);
 			this.ControlBorderedPanel.Size = new System.Drawing.Size(384, 39);
@@ -333,7 +338,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(384, 611);
+			this.ClientSize = new System.Drawing.Size(384, 563);
 			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
 			this.Controls.Add(this.ControlBorderedPanel);
@@ -358,14 +363,12 @@
 		private UI.BorderedPanel ControlBorderedPanel;
 		private System.Windows.Forms.Button OkButton;
 		private System.Windows.Forms.Button CancelButton;
-		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button SelectSource1Button;
 		private System.Windows.Forms.TextBox Source1TextBox;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Button SelectSource2Button;
 		private System.Windows.Forms.TextBox Source2TextBox;
-		private System.Windows.Forms.GroupBox groupBox2;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.TextBox NameTextBox;
 		private System.Windows.Forms.Label label6;
@@ -379,5 +382,7 @@
 		private System.Windows.Forms.Label label8;
 		private System.Windows.Forms.TextBox DataTextBox;
 		private System.Windows.Forms.LinkLabel CreateDiffLink;
+		private UI.GroupPanel groupBox1;
+		private UI.GroupPanel groupBox2;
 	}
 }
