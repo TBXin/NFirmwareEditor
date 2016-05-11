40746380c32f15ad7da57b423f2df4df0ba60c5a
ReikoKitsune <exvoided@gmail.com>
2016-05-11 17:35:16 +0300

    NLog, basic logging configuration, loggin unhandled exceptions to the log.txt file.

--- src/NFirmwareEditor/Program.cs
+++ src/NFirmwareEditor/Program.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Windows.Forms;
 using NFirmwareEditor.Windows;
+using NLog;
 
 namespace NFirmwareEditor
 {
 	internal static class Program
 	{
+		private static readonly Logger s_logger = LogManager.GetCurrentClassLogger();
+
 		/// <summary>
 		/// The main entry point for the application.
 		/// </summary>
 		[STAThread]
 		private static void Main()
 		{
+			AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainWindow());
+		}
+
+		private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs args)
+		{
+			var e = (Exception)args.ExceptionObject;
+			s_logger.Fatal(e);
 		}
 	}
 }
