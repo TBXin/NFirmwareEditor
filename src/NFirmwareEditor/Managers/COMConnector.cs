81d7f8192658d3012449d4ea9ccdc48621420eb0
ReikoKitsune <exvoided@gmail.com>
2016-09-21 17:24:13 +0300

    COM Port Name detection.

---
+++ src/NFirmwareEditor/Managers/COMConnector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Management;
+using JetBrains.Annotations;
+
+namespace NFirmwareEditor.Managers
+{
+	internal class COMConnector
+	{
+		private const string PnpDeviceIdMask = "VID_0416&PID_5020";
+
+		[CanBeNull]
+		public string TryGetPortName()
+		{
+			using (var searcher = new ManagementObjectSearcher(@"SELECT * FROM WIN32_SERIALPORT"))
+			using (var collection = searcher.Get())
+			{
+				foreach (var device in collection)
+				{
+					var pnpDeviceId = (string)device.GetPropertyValue("PNPDeviceID");
+					if (string.IsNullOrEmpty(pnpDeviceId)) continue;
+					if (pnpDeviceId.IndexOf(PnpDeviceIdMask, StringComparison.OrdinalIgnoreCase) == -1) continue;
+
+					var portName = (string)device.GetPropertyValue("DeviceID");
+					return portName;
+				}
+			}
+			return null;
+		}
+	}
+}
